f212bd05b08b21398c954dd4d526a76348d1e881
Alexey Kazakov <alexey3017@mail.ru>
2021-02-22 09:58:44 +0300

    hw2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Messages from './components/Messages'
+import MessageField from "./components/MessageField";
 
 
 function App() {
   return (
     <div className="App">
-        <Messages/>
+        <MessageField/>
     </div>
   );
 }
