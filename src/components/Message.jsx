c3449312d17df53e453d6963253fb73276390229
Alexey Kazakov <alexey3017@mail.ru>
2021-02-28 14:21:21 +0300

    hw3

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,28 @@
 import { useCallback } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  messageArea: {
+    height: "70vh",
+    padding: "10px 0 0 10px",
+  },
+  message: {
+    padding: "10px",
+  },
+  me: {
+    textAlign: "right",
+    paddingRight: "20px",
+  },
+});
 
 function Message(props) {
+  const classes = useStyles();
   const renderMessage = useCallback(
     (message, idx) => (
-      <li key={idx}>
+      <li
+        key={idx}
+        className={classes.message}
+        className={message.author === "Алексей" ? classes.me : null}
+      >
         <h3>{message.author}:</h3>
         <p>{message.text}</p>
       </li>
@@ -12,7 +31,9 @@ function Message(props) {
   );
   return (
     <div>
-      <ul>{props.messages.map(renderMessage)}</ul>
+      <ul className={classes.messageArea}>
+        {props.messages.map(renderMessage)}
+      </ul>
     </div>
   );
 }
